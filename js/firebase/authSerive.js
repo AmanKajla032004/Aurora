ac0d5b46fcbabcef914c95e6098f2f5de0a0ed6f
AmanKajla032004 <daemonaman@gmail.com>
2026-02-26 05:01:18 +0530

    Create authSerive.js

---
+++ js/firebase/authSerive.js
@@ -0,0 +1,42 @@
+import { auth } from "./firebaseConfig.js";
+import {
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+  sendPasswordResetEmail,
+  sendEmailVerification
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const provider = new GoogleAuthProvider();
+
+export function listenToAuthState(callback) {
+  onAuthStateChanged(auth, callback);
+}
+
+export async function register(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
+
+export async function login(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+export async function loginWithGoogle() {
+  return signInWithPopup(auth, provider);
+}
+
+export async function logout() {
+  return signOut(auth);
+}
+
+export async function sendPasswordReset(email) {
+  return sendPasswordResetEmail(auth, email);
+}
+
+export async function verifyEmail() {
+  if (!auth.currentUser) throw new Error("No user signed in");
+  return sendEmailVerification(auth.currentUser);
+}
